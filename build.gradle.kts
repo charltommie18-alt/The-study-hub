696d3751cc2575e8347d144a24803b0a1f3f2757
charltommie18-alt <charltommie18@gmail.com>
2026-09-06 01:39:02 +0200

    Add dependencies for Amazon Appstore SDK and AndroidX

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+
+    implementation(files("libs/amazon-appstore-sdk.jar"))
+
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
+}
